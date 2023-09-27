acb3f6804c432314763c2402d220fc31f1075d35
IshanRattan <ishanrattan12@gmail.com>
2023-09-27 01:02:02 -0700

    define vars and change data type

--- programming/hello.jl
+++ programming/hello.jl
@@ -1,1 +1,13 @@
+# Prints Hello World!
 println("Hello World!")
+
+# Declare variables
+x = 1
+y = 2
+
+println("The sum of x and y is: ", x + y)
+
+# Change data type
+println("Datatype of x is: ", typeof(x))
+x_str = string(x)
+println("x is now of Datatype: ", typeof(x_str))
