660ec0e99c21f2b6b643d18a49d0ac29ce081b97
IshanRattan <ishanrattan12@gmail.com>
2023-10-10 16:07:59 -0700

    string manipulation

---
+++ programming/strings.jl
@@ -0,0 +1,16 @@
+# String manipulation
+
+str = "Hello"
+firstChar = str[1] # Index start from 1
+lastChar = str[end] # end is a special keyword in julia
+firstSlice = str[2:end]
+secondSlice = str[end-2:end]
+
+# Print with interpolation
+println("First char is $firstChar")
+println("Last char is $lastChar")
+println("First slice is $firstSlice")
+println("Second slice is $secondSlice")
+
+# Interpolation with parenthesis
+println("Slice is $(str[1:2])")
