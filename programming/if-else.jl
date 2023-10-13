f75e45cbc610c7113e21dc800a13115dcdb0d199
IshanRattan <ishanrattan12@gmail.com>
2023-10-13 13:08:32 -0700

    if else construct

---
+++ programming/if-else.jl
@@ -0,0 +1,13 @@
+
+
+# Check if n is the correct type
+if typeof(n) == Int64
+    # Print n is the correct type
+    println("n is the correct type")
+    # Use an else statement to print if n is not the correct type
+else
+    println("This script only works on integers")
+    # Finish the conditional statement
+end
+
+println(n)
